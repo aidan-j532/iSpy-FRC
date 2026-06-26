fedfcfa5747aeee306c0f84377048e9efe42a364
Aidan Jensen <aidanrjesen@gmail.com>
2026-06-26 08:55:19 -0500

    Added a change to quatnize paramters

--- iSpy/boot/boot.py
+++ iSpy/boot/boot.py
@@ -550,6 +550,8 @@ def _convert_rknn(pt_file, input_size, dataset_path, task="detect", quantize=Tru
             std_values=[[255, 255, 255]],
             target_platform="rk3588",
             disable_rules=["fuse_exmatmul_add_mul_exsoftmax13_exmatmul_to_sdpa"],
+            quantized_algorithm="kl_divergence",
+            quantized_hybrid_level=3,
         )
         if quantize:
             config_kwargs["quantized_dtype"] = "asymmetric_quantized-8"
