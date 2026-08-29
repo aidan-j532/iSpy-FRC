f66eb3f8840fd2984cddac286d2da9575632304f
Aidan Jensen <aidanrjesen@gmail.com>
2026-08-29 10:14:01 -0500

    Added 5 themes to test.

--- iSpy/config/iSpyConfig.py
+++ iSpy/config/iSpyConfig.py
@@ -198,6 +198,7 @@ class iSpyConfig:
             # "frc" = FRC/WPILib convention: outputs in meters (what robot code
             # expects, matches Limelight/PhotonVision), calibration inputs in inches.
             "unit": "frc",
+            "theme": "carbon",
             "debug_mode": True,
             "frame_sync": False,
             "optimize": False,
